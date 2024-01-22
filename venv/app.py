6b23bcd03ccc99b435dd415f0eeb5e7a89b665fb
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-01-22 14:36:51 -0500

    added port server and fixed quotation marks error

--- venv/app.py
+++ venv/app.py
@@ -4,11 +4,15 @@ import random
 
 quotes = [
     {
-        "quoteText": "You know, Hobbes, some days even my lucky rocket ship underpants don’t help.",
+        "quoteText": "You know, Hobbes, some days even my lucky rocket ship underpants don't help.",
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "Everybody seeks happiness! Not me, though! That’s the difference between me and the rest of the world. Happiness isn’t good enough for me! I demand euphoria!",
+        "quoteText": "What a stupid world",
+        "quoteAuthor": "Calvin"
+    },
+    {
+        "quoteText": "Everybody seeks happiness! Not me, though! That's the difference between me and the rest of the world. Happiness isn't good enough for me! I demand euphoria!",
         "quoteAuthor": "Calvin"
     },
     {
@@ -16,23 +20,23 @@ quotes = [
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "As you can see, I have memorized this utterly useless piece of information long enough to pass a test question. I now intend to forget it forever. You’ve taught me nothing except how to cynically manipulate the system. Congratulations.",
+        "quoteText": "As you can see, I have memorized this utterly useless piece of information long enough to pass a test question. I now intend to forget it forever. You've taught me nothing except how to cynically manipulate the system. Congratulations.",
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "Van Gogh would’ve sold more than one painting if he’d put tigers in them.",
+        "quoteText": "Van Gogh would've sold more than one painting if he'd put tigers in them.",
         "quoteAuthor": "Hobbes"
     },
     {
-        "quoteText": "The world bores you when you’re cool.",
+        "quoteText": "The world bores you when you're cool.",
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "Isn’t it strange that evolution would give us a sense of humour? When you think about it, it’s weird that we have a physiological response to absurdity. We laugh at nonsense. We like it. We think it’s funny. Don’t you think it’s odd that we appreciate absurdity? Why would we develop that way? How does it benefit us?",
+        "quoteText": "Isn't it strange that evolution would give us a sense of humour? When you think about it, it's weird that we have a physiological response to absurdity. We laugh at nonsense. We like it. We think it's funny. Don't you think it's odd that we appreciate absurdity? Why would we develop that way? How does it benefit us?",
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "I suppose if we couldn’t laugh at things that don’t make sense, we couldn’t react to a lot of life.\n(after a long pause) I can’t tell if that’s funny or really scary.",
+        "quoteText": "I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life. I can't tell if that's funny or really scary.",
         "quoteAuthor": "Hobbes and Calvin"
     },
     {
@@ -44,7 +48,7 @@ quotes = [
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "People always make the mistake of thinking art is created for them. But really, art is a private language for sophisticates to congratulate themselves on their superiority to the rest of the world. As my artist’s statement explains, my work is utterly incomprehensible and is therefore full of deep significance.",
+        "quoteText": "People always make the mistake of thinking art is created for them. But really, art is a private language for sophisticates to congratulate themselves on their superiority to the rest of the world. As my artist's statement explains, my work is utterly incomprehensible and is therefore full of deep significance.",
         "quoteAuthor": "Calvin"
     },
     {
@@ -52,7 +56,7 @@ quotes = [
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "They say the world is a stage. But obviously the play is unrehearsed and everybody is ad-libbing his lines.\nMaybe that’s why it’s hard to tell if we’re living in a tragedy or a farce.\nWe need more special effects and dance numbers.",
+        "quoteText": "They say the world is a stage. But obviously the play is unrehearsed and everybody is ad-libbing his lines.Maybe that's why it's hard to tell if we're living in a tragedy or a farce.We need more special effects and dance numbers.",
         "quoteAuthor": "Calvin and Hobbes"
     },
     {
@@ -60,10 +64,9 @@ quotes = [
         "quoteAuthor": "Calvin"
     },
     {
-        "quoteText": "So the secret to good self-esteem is to lower your expectations to the point where they’re already met?",
+        "quoteText": "So the secret to good self-esteem is to lower your expectations to the point where they're already met?",
         "quoteAuthor": "Hobbes"
     }, 
-    
 ]
 
 app = Flask(__name__)
@@ -73,6 +76,7 @@ def generate_random_quote():
     quote = random.choice(quotes)
     return jsonify(quote)
 
-print("Starting the Flask app...")
-
+#the json will run on "http://127.0.0.1:8080/api/quotes/random"
+if __name__ == "__main__":
+    app.run(port=8080) 
 
