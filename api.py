40a9d10ce11d316564f966c70eea4b224db78938
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-01-26 22:05:07 -0500

    Update api.py

--- api.py
+++ api.py
@@ -66,8 +66,185 @@ quotes = [
     {
         "quoteText": "So the secret to good self-esteem is to lower your expectations to the point where they're already met?",
         "quoteAuthor": "Hobbes"
-    }, 
+    },
+    {
+        "quoteText":"Becoming an adult is probably the dumbest thing you could ever do",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"How old do you have to be before you know what's going on?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Know what's weird? Day to day nothing seems to change, but pretty soon everything is different.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Your denial of my victimhood is lowering my self esteem!",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Given that sooner or later, we're all just going to die, what's the point of learning about integers?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"You just go about your business and one day you realize you're not the same person you used to be. People change whether they decide to or not!",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Do you think our morality is defined by our actions, or by what’s inside our hearts?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Isn't it sad how some people's grip on their lives is so precarious that they'll embrace any preposterous delusion, rather than face an occasional bleak truth?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"If you care, you just get disappointed all the time. If you don't care, nothing matters. So you're never upset.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Change is invigorating! If you don't accept new challenges you become complacent and lazy. Your life atrophies!",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"If people sat outside and looked at the stars each night, I'll bet they'd live a lot differently.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"We seem to understand the value of oil, timber, minerals and housing, but not the value of unspoiled beauty, wildlife, solitude and spiritual renewal.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I try to make everyone's day a little more surreal.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I don't care about issues! I've got better things to do than argue with every wrongheaded crackpot with an ignorant opinion! I'm a busy man.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Why does the universe always give you the sign after you do it?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Great experiences are even better when they're shared.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"When a person pauses in mid-sentence to choose a word, that's the best time to jump in and change the subject.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Nothing is permanent, everything changes. That's the one thing we know for sure in this world.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I find my life is easier the lower I keep everyone's expectations.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"What on earth am I doing in here on this beautiful day? This is the only life I've got!",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I tell you Hobbes, it's great to have a friend who appreciates an earnest discussion of ideas.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"We hurl through an incomprehensible darkness. In cosmic terms, we are subatomic particles in a grain of sand on an infinite beach.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"There's never enough time to do all the nothing you want.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Here I am, waiting for the bus. Eleven more years of school to go. Then college, then maybe graduate school, and then I work until I die.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"It's a hard, bitter, cruel world to have to grow up in, Hobbes.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Isn't it weird that one's own past can seem unreal? This is like looking at a picture of someone else.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Oh greatest of the mass media, thank you for elevating emotion, reducing thought, and stifling imagination.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I must say, the future is quite a bit different than I expected.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"It offends the human ego that nature is indifferent to us.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Ya know, sometimes the world seems like a pretty mean place.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"A day can really slip by when you're deliberately avoiding what you're supposed to do.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I like to experience life to the fullest. I say you don't fully appreciate life until you risk losing it.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"I wondered, is it better to do the right thing and fail or is it better to do the wrong thing and succeed?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Did you ever wonder if the person in the puddle is real, and you're just a reflection of him?",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"Given the pace of technology, I propose we leave math to the machines and go play outside.",
+        "quoteAuthor":"Calvin"
+    },
+    {
+        "quoteText":"If good things lasted forever, would we appreciate how precious they are?",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"I suppose if we couldn't laugh at things that don't make sense, we couldn't react to a lot of life.",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"Is it a right to remain ignorant?",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"If you couldn't find any weirdness, maybe we'll just have to make some.",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"If people could put rainbows in zoos, they'd do it!",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"Somehow, it's always right now until it's later.",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"Sometimes one should just look at things and think about things, without doing things.",
+        "quoteAuthor":"Hobbes"
+    },
+    {
+        "quoteText":"I think our actions show what's inside our hearts.",
+        "quoteAuthor":"Hobbes"
+    }
 ]
+
 
 app = Flask(__name__)
 CORS(app)
